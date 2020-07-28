ff365ca2dd7555c123f171c01d12918edf37e41e
Vish Mayer <vish.mayer@gmail.com>
2020-07-28 19:32:38 +0100

    added helper functions for users

--- server/users.js
+++ server/users.js
@@ -1,17 +1,33 @@
 const users = [];
 
-const addUser = () => {
+const addUser = ( { id, name, room }) => {
+  name = name.trim().toLowerCase();
+  name = room.trim().toLowerCase();
+
+  const existingUser = users.find((user) => user.room === room && user.name === name);
+
+  if(existingUser) {
+    return { error: 'Sorry, this username is taken'}
+  }
+
+  const user = { id, name, room };
+
+  users.push(user);
+
+  return { user }
 
 }
 
-const removeUser = () => {
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id ===id);
 
+  if(index !== -1) {
+    return users.splice(index, 1)[0];
+  }
 }
 
-const getUser = () => {
+const getUser = (id) => users.find((user) => user.id === id);
 
-}
+const getUsersInRoom = (room) => users.filter((user) => user.room === room); 
 
-const getUsersInRoom = () => {
-
-}
+module.export = { addUser, removeUser, getUser, getUsersInRoom};
